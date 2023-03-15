7b2070d781f977ba7d61d157753b3d827e89d43e
Danny Zhang <xin.zhang@niu.com>
2023-03-15 17:03:13 +0800

    arr

---
+++ pages/arr.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="arr">
+    {{ arr }}
+    {{ arr2 }}
+    {{ arr3() }}
+  </div>
+</template>
+<script setup>
+const arr = reactive([])
+const target = [1,1,2,2,3,4,2,1,5,6,7,8,6,5,4,7,8,9,10,9,10,0]
+for (const item of target) {
+  if(arr.indexOf(item) === -1) {
+    arr.push(item)
+    arr.sort((a, b)=>{return a-b})
+  }
+}
+const arr2 = [...new Set(target)].sort((a,b)=>{return b-a})
+const arr3 = ()=>{
+  const obj = {}
+  for (const v of target) {
+    obj[v] = v
+  }
+  const w = Object.keys(obj)
+  const key = w.map(Number)
+  console.log(key);
+  // or
+  const key2 = []
+  for (const s of w) {
+    let k2 = Number(s)
+    key2.push(k2)
+  }
+  console.log(key2);
+  return key
+}
+arr3()
+</script>
