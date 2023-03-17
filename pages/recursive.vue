bf19d1992fc1bb5340c986bc8c097a685d476bf9
Danny Zhang <xin.zhang@niu.com>
2023-03-17 15:29:37 +0800

    update

--- pages/recursive.vue
+++ pages/recursive.vue
@@ -39,4 +39,21 @@ function loopRunDG(num){
 }
 let u = ref(loopRunDG(10))
 console.log(u);
+
+/**
+ * 一对兔子生后的两个月后每个月都能生一对小兔子
+ * 计算第N个月的兔子的数量
+ * 1 2 3 4 5 6 7 8  9 10 11 12
+ * 1 1 2 3 5 8 13 21 .....
+ * 规律，当前数等于两个数之和（斐波那契数列）
+ */
+function countNum(month){
+  if(month < 3) {
+    return 1
+  }
+  return countNum(month - 1) + countNum(month - 2)
+}
+let rabitNum = countNum(12)
+console.log('12个月后兔子的数量', rabitNum);
+  
 </script>
